d5a222d37afdcb3167b1cdccabb6e600c8d56ef8
Yonghyuk Jeon <kylejeon83@gmail.com>
2023-01-09 13:58:22 +0900

    test

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
 import streamlit as st
 import psycopg2
 
-# Postgre DB 접속
-connection = psycopg2.connect(
-    host='146.56.169.113',
-    database = 'dct',
-    user = 'yhjeon',
-    password ='Cloud1q2w#$'
-)
-cursor = connection.cursor()
-# cursor.execute("create table test (USER_ID SERIAL PRIMARY KEY, 	USERNAME VARCHAR(50) UNIQUE NOT NULL, 	PASSWORD VARCHAR(50) NOT NULL,	EMAIL VARCHAR(355) UNIQUE NOT NULL,	CREATED_ON TIMESTAMP NOT NULL,	LAST_LOGIN TIMESTAMP)")
+@st.experimental_singleton
+def init_conection():
+    return psycopg2.connect(**st.secrets["postgres"])
+    # Postgre DB 접속
+    # connection = psycopg2.connect(
+    #     host='146.56.169.113',
+    #     database = 'dct',
+    #     # user = 'yhjeon',
+    #     # password ='Cloud1q2w#$'
+    #     user = st.secrets["db_username"],
+    #     password = st.secrets["db_password"]
+    
+conn = init_conection()
+# cursor = connection.cursor()
 # connection.commit()
 
 # data =[100,200,300,400]
 
 # st.bar_chart(data)
 
-modality = cursor.execute("select modality from job group by modality")
-reporter = cursor.execute("select requester_name from job group by requester_name")
+@st.experimental_memo(ttl=660)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+    # modality = cur.execute("select modality from job group by modality")
+    # reporter = cursor.execute("select requester_name from job group by requester_name")
+modality = run_query("select modality from job group by modality;")
+reporter = run_query("select requester_name from job group by requester_name;")
+
 
 st.header("Job Type")
 job_type = st.selectbox('Select a job type',("Requested Job","Completed Job","Scheduled Job","Emergency Job"))
